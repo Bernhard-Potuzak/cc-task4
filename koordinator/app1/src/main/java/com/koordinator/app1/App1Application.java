ab4a266e9c44f79940e78277b615740b33de0323
Artur Veres <artur8118@gmail.com>
2020-01-26 16:46:02 +0200

    Adding proxying

--- koordinator/app1/src/main/java/com/koordinator/app1/App1Application.java
+++ koordinator/app1/src/main/java/com/koordinator/app1/App1Application.java
@@ -2,74 +2,100 @@ package com.koordinator.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class App1Application {
 
-	public static String[] WorkerAddresses = {
-			"http://localhost:10101/",
-			"http://localhost:10102/",
-			"http://localhost:10103/",
-			"http://localhost:10104/"
-	};
+    public static String[] WorkerAddresses = {
+            "http://localhost:10101/",
+            "http://localhost:10102/",
+            "http://localhost:10103/",
+            "http://localhost:10104/"
+    };
 
-	public static String[] AssignedAddresses = new String[16];
+    public static String[] AssignedAddresses = new String[16];
 
-	public static void setAssignedAddresses() {
-		for (int i = 0; i < AssignedAddresses.length; i++) {
-			if (i % 4 == 0) {
-				AssignedAddresses[i] = WorkerAddresses[i % 4];
-			}
-		}
-	}
+    public static void setAssignedAddresses() {
+        for (int i = 0; i < AssignedAddresses.length; i++) {
+            if (i % 4 == 0) {
+                AssignedAddresses[i] = WorkerAddresses[i % 4];
+            }
+        }
+    }
 
-	public static String computeAddress(String key) {
-		long hash = new FNVHash().hash(key.getBytes());
-		int initialIndex = (int) (hash % 16);
-		while (AssignedAddresses[initialIndex] == null) {
-			if (initialIndex != 0) {
-				initialIndex -= 1;
-			} else {
-				initialIndex = 16;
-			}
-		}
-		return AssignedAddresses[initialIndex];
-	}
+    public static String computeAddress(String key) {
+        long hash = new FNVHash().hash(key.getBytes());
+        int initialIndex = (int) (hash % 16);
+        while (AssignedAddresses[initialIndex] == null) {
+            if (initialIndex != 0) {
+                initialIndex -= 1;
+            } else {
+                initialIndex = 16;
+            }
+        }
+        return AssignedAddresses[initialIndex];
+    }
 
-	public static void main(String[] args) {
-		setAssignedAddresses();
-		SpringApplication.run(App1Application.class, args);
-	}
+    public static String getResponse(String address, Map<String, Object> payload, HttpServletRequest request) throws URISyntaxException {
+        try {
+            URL url = new URL(address);
+            URI uri = new URI(url.getProtocol(), null, url.getHost(), url.getPort(), request.getRequestURI(), request.getQueryString(), null);
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<Map<String, Object>>(payload), String.class);
+            return responseEntity.getBody();
+        } catch (MalformedURLException e) {
+            return "";
+        }
+    }
 
-	@RequestMapping(value = "/insert", method = RequestMethod.POST)
-	public void insert(@RequestBody Map<String, Object> payload){
-		//TODO: implement insert
-	}
+    public static void main(String[] args) {
+        setAssignedAddresses();
+        SpringApplication.run(App1Application.class, args);
+    }
 
-	@RequestMapping(value = "/delete", method = RequestMethod.POST)
-	public void delete(@RequestBody Map<String, Object> payload){
-		//TODO: implement delete
-	}
-	@RequestMapping(value = "/search", method = RequestMethod.POST)
-	public void search(@RequestBody Map<String, Object> payload){
-		//TODO: implement search
-	}
-	@RequestMapping(value = "/range", method = RequestMethod.POST)
-	public void range(@RequestBody Map<String, Object> payload){
-		//TODO: implement range
-	}
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    @ResponseBody
+    public String insert(@RequestBody Map<String, Object> payload, HttpServletRequest request) throws URISyntaxException {
+        String address = "http://localhost:10101/";
+        return getResponse(address, payload, request);
+    }
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test(){
-		//TODO: implement range
-		return "Koordinator is running";
-	}
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String delete(@RequestBody Map<String, Object> payload, HttpServletRequest request) throws URISyntaxException {
+        String address = "http://localhost:10101/";
+        return getResponse(address, payload, request);
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String search(@RequestBody Map<String, Object> payload, HttpServletRequest request) throws URISyntaxException {
+        String address = "http://localhost:10101/";
+        return getResponse(address, payload, request);
+    }
+
+    @RequestMapping(value = "/range", method = RequestMethod.POST)
+    public String range(@RequestBody Map<String, Object> payload, HttpServletRequest request) throws URISyntaxException {
+        String address = "http://localhost:10101/";
+        return getResponse(address, payload, request);
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        //TODO: implement range
+        return "Koordinator is running";
+    }
 
 }
